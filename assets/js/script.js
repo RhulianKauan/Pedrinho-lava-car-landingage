16b793bf2f73cf07541ab0e7e5e08a36161f26c1
Rhulian Kauan <rhulian00@gmail.com>
2022-02-21 10:33:25 -0300

    salvo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,18 @@
 const btnMobile = document.querySelector('#menu__icon')
       
-
-
-
 function toggleMenu() {
     const nav = document.querySelector('#nav');
     nav.classList.toggle('active');
 }
 
-
 btnMobile.addEventListener('click', toggleMenu);
+
+
+// togge list
+
+const toggeList = document.querySelectorAll('.contentBx')
+toggeList.forEach((item)=> {
+    item.addEventListener('click', ()=> {
+        item.classList.toggle('active')
+    })
+})
